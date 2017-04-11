dee4ea06cf2541bcd602078e7b1c58f30fcf1272
Kenan Rhoton <kenan.rhoton@gmail.com>
2017-04-11 23:13:40 +0200

    Parse into DOM

--- parser/dom.go
+++ parser/dom.go
@@ -10,21 +10,64 @@ func (p *Parser) ParseDOM() *dom.Node {
 	return doc
 }
 
-func (p *Parser) MakeChildren(n *dom.Node) {
+func (p *Parser) MakeChildren(d *dom.Node) {
 	for {
 		p.Whitespace()
-		switch n := p.Next(); {
+		switch n,_ := p.Next(); {
 		case n == '<':
+                    _,_ = p.Eat()
+                    if i,_ := p.Next(); i == '/' {
+                        _,_ = p.Eat()
+                        _,_ = p.EatTagName()
+                        p.EatTagClosed()
+                        return
+                    }
+                    tag, _ := p.EatTagName()
+                    if len(tag) > 0 {
+                        attr, _ := p.EatAttributes()
+                        e := dom.Elem(string(tag), attr, make([]*dom.Node,0,0))
+                        if p.EatTagClosed() == false {
+                            p.MakeChildren(e)
+                        }
+                        d.Append(e)
+                    }
 		default:
 			t, err := p.EatText()
 			if len(t) > 0 {
-				n.Append(Text(t))
+				d.Append(dom.Text(string(t)))
 			}
 			if err != nil {
 				if err.Error() == "end of file" {
-					break
+                                    return
 				}
 			}
 		}
 	}
+}
+
+func (p *Parser) EatAttributes() (map[string]string, error) {
+    p.Whitespace()
+    res := make(map[string]string)
+    for {
+        if n, err := p.Next(); n == '>' || n == '/' {
+            return res, err
+        }
+        name, err := p.EatAttrName()
+        if len(name) == 0 {
+            return res, err
+        }
+        value, _ := p.EatAttrValue()
+        res[string(name)] = string(value)
+    }
+}
+
+func (p *Parser) EatTagClosed() bool {
+    res := false
+    for r, err := p.Eat(); r != '>' && err == nil; {
+        if r == '/' {
+            res = true
+        }
+        r, err = p.Eat()
+    }
+    return res
 }
--- parser/dom_test.go
+++ parser/dom_test.go
@@ -25,3 +25,27 @@ func ExampleOneLine() {
 	//   </html>
 	// </document>
 }
+
+func ExampleSpaced() {
+	testdata := "   \t  <html>\n\n \t<head><title>     YO!</title></head><body   ><h1>lol</h1>dis<elem /></body></html>                     "
+	p := Load(testdata)
+	d := p.ParseDOM()
+	d.PrettyPrint()
+	// Output:
+	// <document>
+	//   <html>
+	//     <head>
+	//       <title>
+	//         YO!
+	//       </title>
+	//     </head>
+	//     <body>
+	//       <h1>
+	//         lol
+	//       </h1>
+	//       dis
+	//       <elem/>
+	//     </body>
+	//   </html>
+	// </document>
+}
