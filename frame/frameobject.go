8c14f9639c72d49a6c5af18447f6b5aa90ac19f5
Kenan Rhoton <kenan.rhoton@gmail.com>
2017-04-18 20:29:40 +0200

    Frames: Phase 2

--- frame/frameobject.go
+++ frame/frameobject.go
@@ -3,19 +3,24 @@ package frame
 import (
 	"github.com/kenan-rhoton/kraal/dom"
 	"github.com/kenan-rhoton/kraal/parser"
+	"strings"
 )
 
 func BuildObject(n *dom.Node) *FrameObject {
 	o := &FrameObject{}
-	o.Class = n.Type()
+	o.Tag = n.Type()
 	o.Attributes = n.Attrs()
 	o.Styles = make(map[string]string)
 	if o.Attributes["style"] != "" {
-		css := parser.ParseCSS(o.Attributes["style"])
-		for _, r := range css.Rules {
-			for _, d := range r.Declare {
+		p := parser.Load(o.Attributes["style"])
+		dec, err := parser.EatDeclarations(p)
+		if err == nil {
+			for _, d := range dec {
 				o.ApplyStyleLocal(d)
 			}
+		}
+		if o.Attributes["class"] != "" {
+			o.Classes = strings.Split
 		}
 	}
 	o.Children = make([]*FrameObject, 0)
@@ -29,13 +34,84 @@ func (o *FrameObject) ApplyStyleLocal(d parser.Declaration) {
 	o.Styles[d.Name()] = d.Value()
 }
 
-func (o *FrameObject) ApplyStyles(css *parser.StyleSheet) {
+func (o *FrameObject) ApplyStyleRecursive(d parser.Declaration) {
+	o.ApplyStyleLocal(d)
+        for _, c := range o.Children {
+            o.ApplyStyleRecursive(d)
+        }
+}
 
+func (o *FrameObject) MatchSelector(sel *parser.Selector) bool {
+	if sel.ID != "" && sel.ID != o.ID {
+		return false
+	}
+	if sel.Tag != "" && sel.Tag != o.Tag {
+		return false
+	}
+	if sel.Classes != nil {
+		if o.Classes == nil {
+			return false
+		}
+		for _, c := range sel.Classes {
+			found = false
+			for _, find := range o.Classes {
+				if c == find {
+					found = true
+					break
+				}
+			}
+			if !found {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (o *FrameObject) ApplyStyleNow(r *parser.Rule) {
+	if o.MatchSelector(r.Select) {
+		if r.Select.Child != nil {
+			for _, c := range o.Children {
+				c.ApplyStyleNow(r.Select.Child)
+			}
+		} else if r.Select.Descendant != nil {
+			for _, c := range o.Children {
+				c.ApplyStyle(r.Select.Descendant)
+			}
+		} else {
+			for _, dec := range r.Declare {
+				if isInheritable(dec) {
+					o.ApplyStyleRecursive(dec)
+				} else {
+					o.ApplyStyleLocal(dec)
+				}
+			}
+		}
+		return true
+	} else {
+		return false
+	}
+}
+
+func (o *FrameObject) ApplyStyle(r *parser.Rule) {
+	if o.ApplyStyleNow(r) == false {
+		for _, c := range o.Children {
+			c.ApplyStyle(r)
+		}
+	}
+}
+
+func (o *FrameObject) ApplyStyles(css *parser.StyleSheet) {
+	for _, r := range css.Rules {
+		o.ApplyStyle(r)
+	}
 }
 
 type FrameObject struct {
-	Class      string
+	Tag        string
 	Attributes map[string]string
+	Classes    []string
+	ID         string
 	Styles     map[string]string
 	Children   []*FrameObject
 }
